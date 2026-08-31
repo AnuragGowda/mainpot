9634f1606d7e1eeea6001829e1e8d931d242757a
Anurag Gowda <gowdaanuragr@gmail.com>
2026-08-30 20:16:55 -0500

    chore: update Mainpot repository link

--- lib/product.ts
+++ lib/product.ts
@@ -1,4 +1,3 @@
-export const GITHUB_URL = "https://github.com/AnuragGowda/ante";
+export const GITHUB_URL = "https://github.com/AnuragGowda/mainpot";
 export const BUG_REPORT_URL = `${GITHUB_URL}/issues/new?template=bug_report.yml`;
 export const FEATURE_REQUEST_URL = `${GITHUB_URL}/issues/new?template=feature_request.yml`;
-
